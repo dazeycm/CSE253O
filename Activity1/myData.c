b918e2f0dee013c584ad18f31711679c5fecbd51
Craig Dazey <dazeycm@miamioh.edu>
2015-01-05 11:55:18 -0500

    Added first activity

---
+++ Activity1/myData.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+  printf("Name: Craig\n");
+  printf("Major: Software Engineering\n");
+  printf("Hometown: Tiffin, Ohio\n");
+}
---
+++ Activity1/myData.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//name, major, hometown
+int main() {
+  
+  NSLog(@"Name: Craig Dazey\n");
+  NSLog(@"Major: Software Engineering\n");
+  NSLog(@"Hometown: Tiffin, Ohio"); 
+}
