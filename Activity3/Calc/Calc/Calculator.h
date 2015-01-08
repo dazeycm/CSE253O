edfa0ff6dbbf08e4d17d2906f6c0d243eb6708f1
Craig Dazey <dazeycm@miamioh.edu>
2015-01-08 11:08:34 -0500

    Added calculator class

---
+++ Activity3/Calc/Calc/Calculator.h
@@ -0,0 +1,13 @@
+//
+//  Calculator.h
+//  Calc
+//
+//  Created by Dazey, Craig Michael Jr. on 1/8/15.
+//  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+@end
---
+++ Activity3/Calc/Calc/Calculator.m
@@ -0,0 +1,13 @@
+//
+//  Calculator.m
+//  Calc
+//
+//  Created by Dazey, Craig Michael Jr. on 1/8/15.
+//  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
+//
+
+#import "Calculator.h"
+
+@implementation Calculator
+
+@end
