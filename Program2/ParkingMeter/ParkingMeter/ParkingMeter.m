06aea14e162d6f20233cfe846ab515577c16c0ad
Craig Dazey <dazeycm@miamioh.edu>
2015-01-08 16:00:13 -0500

    Documentation and don't let time go below 0

--- Program2/ParkingMeter/ParkingMeter/ParkingMeter.m
+++ Program2/ParkingMeter/ParkingMeter/ParkingMeter.m
@@ -12,6 +12,8 @@
 
 @synthesize cash, nickels, dimes, quarters, time;
 
+
+//  Returns false if time would exceed 90 minutes, and true otherwise
 -(bool) insertNickels: (int) num    {
     if(time + 10 * num > 90)
         return false;
@@ -38,23 +40,25 @@
     cash += .25 * num;
     return true;
 }
-
+//  Returns the amount of cash in the machine and then resets it
 -(float) emptyCash  {
     float profit = cash;
     cash = 0;
     return profit;
 }
-
+//  Prints the amount of cash inserted in total, the remaining time, and how much money is currently in the machine
 -(void) print   {
     printf("\tYou've inserted $%.2f\n\tYou have %d remaining minutes.\n\tThere is $%.2f currently in the machine\n\n", (nickels * .05 + dimes * .1 + quarters * .25), time, cash);
 }
-
+//  Returns true if there is still time on the meter, and false otherwise
 -(bool) timePasses: (int) minutes   {
     time -= minutes;
-    if(minutes > 0)
-        return true;
-    else
+    if(minutes < 0) {
+        minutes = 0;
         return false;
+    }
+    else
+        return true;
 }
 
 @end
