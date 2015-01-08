c846fbaa68a59187344e7da7aca6ff75c6f41689
Craig Dazey <dazeycm@miamioh.edu>
2015-01-08 10:32:53 -0500

    Added activity3

---
+++ Activity3/Calc/Calc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calc
+//
+//  Created by Dazey, Craig Michael Jr. on 1/8/15.
+//  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
