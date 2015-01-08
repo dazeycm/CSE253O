776517398ac2ad38bbe2dd179c72cb2742c950df
Craig Dazey <dazeycm@miamioh.edu>
2015-01-08 15:19:53 -0500

    added parkingmeter imp class

---
+++ Program2/ParkingMeter/ParkingMeter/ParkingMeter.m
@@ -0,0 +1,39 @@
+//
+//  ParkingMeter.m
+//  ParkingMeter
+//
+//  Created by Dazey, Craig Michael Jr. on 1/8/15.
+//  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
+//
+
+#import "ParkingMeter.h"
+
+@implementation ParkingMeter
+
+@synthesize cash, nickels, dimes, quarters, time;
+
+-(bool) insertNickels: (int) num    {
+    
+}
+
+-(bool) insertDimes: (int) num  {
+    
+}
+
+-(bool) insertQuarters: (int) num   {
+    
+}
+
+-(float) emptyCash  {
+    
+}
+
+-(void) print   {
+    
+}
+
+-(bool) timePasses: (int) minutes   {
+    
+}
+
+@end
