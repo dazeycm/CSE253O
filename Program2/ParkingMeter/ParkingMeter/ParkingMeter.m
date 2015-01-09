94961205b15f4cf84ef546290f2ba10f0d51a073
Craig Dazey <dazeycm@miamioh.edu>
2015-01-09 12:31:31 -0500

    Corrected boolean output of methods, added helper methods

--- Program2/ParkingMeter/ParkingMeter/ParkingMeter.m
+++ Program2/ParkingMeter/ParkingMeter/ParkingMeter.m
@@ -13,25 +13,46 @@
 
 @synthesize cash, nickels, dimes, quarters, time;
 
+const static int MAXTIME = 90;
 
-//  Returns false if time would exceed 90 minutes, and true otherwise
+//if time > 90, sets time to 90
+-(void) correctTime {
+    if(time > MAXTIME)
+        time = MAXTIME;
+}
+//returns true if num is >= 0
+-(bool) biggerThanZero: (int) num  {
+    if(num < 0)
+        return false;
+    else
+        return true;
+}
 -(bool) insertNickels: (int) num    {
+    if(![self biggerThanZero: num])
+        return false;
     nickels += num;
     time += 10 * num;
+    [self correctTime];
     cash += .05 * num;
     return true;
 }
 
 -(bool) insertDimes: (int) num  {
+    if(![self biggerThanZero: num])
+        return false;
     dimes += num;
     time += 20 * num;
+    [self correctTime];
     cash += .1 * num;
     return true;
 }
 
 -(bool) insertQuarters: (int) num   {
+    if(![self biggerThanZero: num])
+        return false;
     quarters += num;
     time += 60 * num;
+    [self correctTime];
     cash += .25 * num;
     return true;
 }
@@ -47,6 +68,8 @@
 }
 //  Returns true if there is still time on the meter, and false otherwise
 -(bool) timePasses: (int) minutes   {
+    if(![self biggerThanZero: minutes])
+        return false;
     time -= minutes;
     if(time <= 0) {
         time = 0;
