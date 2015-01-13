21a9d0afe2d3173beedc814a75c4766f9e4adcf7
Craig Dazey <dazeycm@miamioh.edu>
2015-01-13 12:54:24 -0500

    Still not working

--- Program3/AddressBook/AddressBook/main.m
+++ Program3/AddressBook/AddressBook/main.m
@@ -43,7 +43,7 @@ void findData() { //TODO: Check to see if name is in dictionary
     printf("Enter a name: ");
     char name[20];
     fgets(name, 20, stdin);
-    if ([myDict objectForKey: [NSString stringWithUTF8String:name]] != nil)  {
+    if ([myDict objectForKey:[NSString stringWithUTF8String:name]])  {
         printf("IT WORKED JESUS");
     }
     else    {
