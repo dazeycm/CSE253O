e9f947e95ea3c459818196d6a8ee7a73d924c555
Craig Dazey <dazeycm@miamioh.edu>
2015-01-14 18:26:43 -0500

    Finished docuementation

--- Program3/AddressBook/AddressBook/main.m
+++ Program3/AddressBook/AddressBook/main.m
@@ -6,21 +6,26 @@
 //  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
 //
 //  Purpose: Practice using ObjC container classes to develop an address book
+//  Below you'll see a removeNewLine function I made to take care of the new line problem when using fgets
+//  However, I think it's easier to convert the user input into an NSString first (which I'd do anyways to store it in the addressdata)
+//  and then you can use replace method of NSString which is stringByReplacingOccurenceOfString: withString which is a lot nicer I think
 
 #import <Foundation/Foundation.h>
 #import "AddressData.h"
 
 NSMutableDictionary* myDict;
 
-NSString* removeNewLine(NSString* input)   {
-    char* str = (char*)[input UTF8String];
-    long i = strlen(str)-1;
-    if( str[i] == '\n')
-        str[i] = '\0';
-    input = [NSString stringWithUTF8String:str];
-    return input;
-}
+//  Old new line removal method
+//NSString* removeNewLine(NSString* input)   {
+//    char* str = (char*)[input UTF8String];
+//    long i = strlen(str)-1;
+//    if( str[i] == '\n')
+//        str[i] = '\0';
+//    input = [NSString stringWithUTF8String:str];
+//    return input;
+//}
 
+//  Reads in info from the file, and places it in the dictionary
 void initDict()    {
     FILE *file;
     myDict = [[NSMutableDictionary alloc]init];
@@ -41,7 +46,7 @@ void initDict()    {
         NSString* name = [parts objectAtIndex:0];
         NSString* email = [parts objectAtIndex:1];
         NSString* tele = [parts objectAtIndex:2];
-        tele = [tele stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+        tele = [tele stringByReplacingOccurrencesOfString:@"\n" withString:@""];    //New newline removal method
         
         [data setEmail: email andPhone: tele];
         [myDict setObject:data forKey:name];
@@ -52,8 +57,9 @@ void findData() {
     printf("Enter a name: ");
     char userName[20];
     fgets(userName, 20, stdin);
-    NSString* name = removeNewLine([NSString stringWithUTF8String:userName]);
+    NSString* name = [[NSString stringWithUTF8String:userName] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
     
+    // returns true if the object was found in the dictionary
     if ([myDict objectForKey:name])  {
         AddressData* data = [myDict objectForKey:name];
         printf("***********************************************************\n\t\tEmail address: %s\n\t\tTelephone Number: %s\n***********************************************************\n", [[data email] UTF8String], [[data phone] UTF8String]);
@@ -67,7 +73,7 @@ void addAddressData()  {
     printf("Enter a name: ");
     char userName[20];
     fgets(userName, 20, stdin);
-    NSString* name = removeNewLine([NSString stringWithUTF8String:userName]);
+    NSString* name = [[NSString stringWithUTF8String:userName] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
     
     if ([myDict objectForKey:name]) {
         printf("Error: Name in address book!\n");
@@ -77,39 +83,41 @@ void addAddressData()  {
     printf("Enter an email: ");
     char userEmail[50];
     fgets(userEmail, 50, stdin);
-    NSString* email = removeNewLine([NSString stringWithUTF8String:userEmail]);
+    NSString* email = [[NSString stringWithUTF8String:userEmail] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
     
     printf("Enter a telephone: ");
     char userTele[20];
     fgets(userTele, 20, stdin);
-    NSString* tele = removeNewLine([NSString stringWithUTF8String:userTele]);
-
+    NSString* tele = [[NSString stringWithUTF8String:userTele] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+    
     AddressData* dataForDict  = [[AddressData alloc]init];
     [dataForDict setEmail:email andPhone:tele];
     
     [myDict setObject:dataForDict forKey:name];
 }
 
-void editData()    { //TODO: Everything pretty much
+void editData()    {
     printf("Enter a name: ");
     char userName[20];
     fgets(userName, 20, stdin);
-    NSString* name = removeNewLine([NSString stringWithUTF8String:userName]);
+    NSString* name = [[NSString stringWithUTF8String:userName] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
     
     if ([myDict objectForKey:name]) {
         printf("Enter an email: ");
         char userEmail[50];
         fgets(userEmail, 50, stdin);
-        NSString* email = removeNewLine([NSString stringWithUTF8String:userEmail]);
+        NSString* email = [[NSString stringWithUTF8String:userEmail] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
         
         printf("Enter a telephone: ");
         char userTele[20];
         fgets(userTele, 20, stdin);
-        NSString* tele = removeNewLine([NSString stringWithUTF8String:userTele]);
+                NSString* tele = [[NSString stringWithUTF8String:userTele] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
         
         AddressData* dataForDict  = [[AddressData alloc]init];
         [dataForDict setEmail:email andPhone:tele];
         
+        //Remove old key value pair and insert new one
+        //May not have to remove old key value, you could just overwrite it, however the documentation wasn't clear
         [myDict removeObjectForKey:name];
         [myDict setObject:dataForDict forKey:name];
     }
@@ -130,7 +138,7 @@ int main(int argc, const char * argv[])
             printf("Enter your choice: ");
             char number[50];
             fgets(number, 50, stdin);
-            choice = atoi(number);
+            choice = atoi(number);  //converts chars to an int, returns 0 if it fails which would go to default, so it's safe
             switch(choice)  {
                 case 1:
                     findData();
@@ -142,7 +150,7 @@ int main(int argc, const char * argv[])
                     editData();
                     break;
                 case 4:
-                    printf("Adios!");
+                    printf("Adios!\n");
                     break;
                 default:
                     printf("Invalid input!\n");
