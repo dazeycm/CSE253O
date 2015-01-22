6ccd05ff1fb13ef807d7b5fbfe5a6c40cc56e5d6
dazeycm <dazeycm@miamioh.edu>
2015-01-22 15:32:54 -0500

    Added background class

---
+++ TermProject/DripDrop/DripDrop/BackgroundChooser.h
@@ -0,0 +1,13 @@
+//
+//  BackgroundChooser.h
+//  DripDrop
+//
+//  Created by Dazey, Craig Michael Jr. on 1/22/15.
+//  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BackgroundChooser : NSObject
+
+@end
---
+++ TermProject/DripDrop/DripDrop/BackgroundChooser.m
@@ -0,0 +1,13 @@
+//
+//  BackgroundChooser.m
+//  DripDrop
+//
+//  Created by Dazey, Craig Michael Jr. on 1/22/15.
+//  Copyright (c) 2015 Dazey, Craig Michael Jr. All rights reserved.
+//
+
+#import "BackgroundChooser.h"
+
+@implementation BackgroundChooser
+
+@end
